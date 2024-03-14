1ce8d7301fdb29491a9b78ebb7deb1b6417fe29f
Shiaupiau <stu43005@gmail.com>
2024-03-14 15:58:40 +0800

    feat: add logs for agenda

--- src/modules/schedule.ts
+++ src/modules/schedule.ts
@@ -5,10 +5,24 @@ import { MONGO_URI } from "./db";
 export function getAgenda() {
   assert(MONGO_URI, "MONGO_URI should be defined.");
 
-  return new Agenda({
+  const agenda = new Agenda({
     db: {
       address: MONGO_URI,
       // collection: isProd ? "agendaJobs" : `testJobs-${HOSTNAME}`,
     },
   });
+
+  agenda.on("start", (job) => {
+    console.log(`[${job.attrs.name}] starting at ${new Date()}`);
+  });
+
+  agenda.on("success", (job) => {
+    console.log(`[${job.attrs.name}] successed at ${new Date()}`);
+  });
+
+  agenda.on("fail", (err, job) => {
+    console.log(`[${job.attrs.name}] failed with error: ${err.message}`);
+  });
+
+  return agenda;
 }
